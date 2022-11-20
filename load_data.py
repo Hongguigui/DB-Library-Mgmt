d8ba7c2e02f270308e792d24f52589e14042a5f4
Xiao Lin <xiaolin1538@gmail.com>
2022-11-20 17:52:42 -0500

    Updated the book csv file for db usage

--- load_data.py
+++ load_data.py
@@ -9,7 +9,7 @@ def load_data():
 	pwd = 'root'
 
 	# Create dataframe
-	book_df = pd.read_csv('dbbooks.csv', index_col=0)
+	book_df = pd.read_csv('dbbooks.csv')
 
 	# Create SQLAlchemy engine to connect to MySQL Database
 	engine = create_engine('mysql+pymysql://{user}:{pw}@{host}/{db}'.format(host=hostname, db=dbname, user=uname, pw=pwd))
