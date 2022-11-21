0c4b1929a3b4ee0e70c578ce656b75eb8d244540
Will Wu <hwu8609@bths.edu>
2022-11-20 22:13:08 -0500

    Initial Get API and data filtering

--- app.py
+++ app.py
@@ -5,23 +5,32 @@ from api.HelloApiHandler import HelloApiHandler
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
+from flask_marshmallow import Marshmallow
+from pprint import pprint
 import pymysql
-from load_data import load_data
+# from load_data import load_data
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 #CORS(app) #comment this on deployment
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost:3306/library'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/library'
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+# def Convert(lst):
+#     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+#     return res_dct
 
 
 class Book(db.Model):
     __tablename__ = "books"
-    ISBN = db.Column(db.Integer, primary_key=True)
+    isbn13 = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
-    author = db.Column(db.String(50))
-    categories = db.Column(db.Integer)
+    authors = db.Column(db.String(50))
+    categories = db.Column(db.String(50))
+    thumbnail = db.Column(db.String(200))
     averageRating = db.Column(db.Float)
     yearPublished = db.Column(db.Integer)
 
@@ -30,47 +39,72 @@ class Book(db.Model):
         db.session.commit()
         return self
 
-    def __init__(self,ISBN,title,author,categories,averageRating,yearPublished):
-        self.ISBN = ISBN
-        self.title = title
-        self.author = author
-        self.categories = categories
-        self.averageRating = averageRating
-        self.yearPublished = yearPublished
+    # def __init__(self,isbn13,title,authors,categories,thumbnail,averageRating,yearPublished):
+    #     self.isbn13 = isbn13
+    #     self.title = title
+    #     self.authors = authors
+    #     self.categories = categories
+    #     self.thumbnail = thumbnail
+    #     self.averageRating = averageRating
+    #     self.yearPublished = yearPublished
 
     def __repr__(self):
-        return '' % self.ISBN
+        return '' % self.isbn13
 
 
-class BookSchema(ModelSchema):
+class BookSchema(ma.Schema):
     class Meta(ModelSchema.Meta):
-        model = Book
-        sqla_session = db.session
-    ISBN = fields.Number(dump_only=True)
-    title = fields.String(required=True)
-    author = fields.String(required=False)
-    categories = fields.String(required=False)
-    averageRating = fields.Number(required=False)
-    yearPublished = fields.Number(required=False)
+        fields = ("isbn13", "title", "authors", "categories", "categories", "thumbnail", "averageRating", "yearPublished")
+    #     model = Book
+    #     sqla_session = db.session
+    # ISBN = fields.Number(dump_only=True)
+    # title = fields.String(required=True)
+    # authors = fields.String(required=False)
+    # categories = fields.String(required=False)
+    # thumbnail = fields.String(required=False)
+    # averageRating = fields.Number(required=False)
+    # yearPublished = fields.Number(required=False)
 
+
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
+
+
+class BookListResource(Resource):
+    def get(self):
+        books = Book.query.all()
+        return books_schema.dump(books)
+
+    # def post(self):
+    #     new_post = Post(
+    #         title=request.json['title'],
+    #         content=request.json['content']
+    #     )
+    #     db.session.add(new_post)
+    #     db.session.commit()
+    #     return post_schema.dump(new_post)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 
-@app.route("/books", methods=['GET'])
-def index():
-    get_books = Book.query.all()
-    book_schema = BookSchema(many=True)
-    books = book_schema.dump(get_books)
-    return make_response(jsonify({"books": books}))
+# @app.route("/books")
+# def index():
+#     get_books = Book.query.all()
+#     return jsonify(get_books)
+    # get_books = Book.query.all()
+    # print(get_books)
+    # book_schema = BookSchema(many=False)
+    # books = book_schema.dump(get_books)
+    # return make_response(jsonify({"books": books}))
 
 
 api.add_resource(HelloApiHandler, '/flask/hello')
-
+api.add_resource(BookListResource, '/books')
 
 
 if __name__ == "__main__":
-    load_data()
+    # load_data()
     app.run()
+
