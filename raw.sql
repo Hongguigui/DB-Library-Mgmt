5fe5956a8316551b60891e274095d2fb22602109
Sophia Xu <sophiahsu1017@gmail.com>
2022-12-15 01:15:04 -0500

    update raw sql

--- raw.sql
+++ raw.sql
@@ -3,5 +3,5 @@ on schedule every 1 day do
 	update borrows
 	set timeLeft = timeLeft - 1
 
--- run this to disable evenet
--- alter event autoDayUpdate disable
+-- run this to disable event
+-- alter event minusOneSecond disable
