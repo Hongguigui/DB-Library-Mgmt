fcb86deaefa91d7da25dd85f58a7a424f676a03a
Sophia Xu <sophiahsu1017@gmail.com>
2022-12-18 15:21:15 -0500

    Added markLate event

--- raw.sql
+++ raw.sql
@@ -3,5 +3,10 @@ on schedule every 1 day do
 	update borrows
 	set timeLeft = timeLeft - 1
 
+create event markLate
+on schedule every 1 hour do
+	update borrows set late = 1 where borrows.timeLeft < 0
+
 -- run this to disable event
 -- alter event minusOneSecond disable
+-- alter event markLate disable
